e38459b00a379f1b2d67b94be7f1d116550c7101
Kimberly Grey <kimandthemachine@gmail.com>
2022-07-03 16:11:26 +0100

    :sparkles: Add theme switcher

--- assets/js/theme-switcher.js
+++ assets/js/theme-switcher.js
@@ -2,48 +2,64 @@ class ThemeSwitcher {
   constructor() {
     this.$toggle = document.querySelector("[data-theme-toggle]");
 
+    this.query = window.matchMedia("(prefers-color-scheme: dark)");
     this.themePreference = localStorage.getItem("theme-preference") || "auto";
     this.themeOptions = {
       auto: "🤖 Auto",
-      dark: "🌘 Dark mode",
-      light: "☀️ Light mode",
+      dark: "🌜 Always dark",
+      light: "☀️ Always light",
     };
 
     this.onLoad();
   }
   onLoad() {
-    const query = window.matchMedia("(prefers-color-scheme: dark)");
-
     const loadCheck = () => {
       if (localStorage.getItem("theme-preference") !== "auto") {
         return this.setCurrentTheme(localStorage.getItem("theme-preference"));
       }
 
-      return query.matches
+      return this.query.matches
         ? this.setCurrentTheme("dark")
         : this.setCurrentTheme("light");
     };
 
-    query.addEventListener("change", loadCheck);
+    this.query.addEventListener("change", loadCheck);
     loadCheck();
 
+    this.$toggle.bindClick = this.onClick.bind(this);
+    this.$toggle.addEventListener("click", this.$toggle.bindClick);
+
+    this.$toggle.setAttribute("aria-live", "polite");
     this.$toggle.removeAttribute("hidden");
     this.setToggleState(this.themePreference);
   }
   onClick(e) {
-    // TODO
+    if (this.themePreference === "auto") {
+      // Set to dark
+      this.setToggleState("dark");
+      this.setCurrentTheme("dark");
+    } else if (this.themePreference === "dark") {
+      // Set to light
+      this.setToggleState("light");
+      this.setCurrentTheme("light");
+    } else {
+      // Set to auto
+      this.setToggleState("auto");
+      this.query.matches
+        ? this.setCurrentTheme("dark")
+        : this.setCurrentTheme("light");
+    }
   }
   setCurrentTheme(theme) {
-    const d = document.documentElement;
     localStorage.setItem("theme", theme);
-    d.setAttribute("data-theme", theme);
+    document.documentElement.setAttribute("data-theme", theme);
   }
-  setToggleState(state) {
-    this.$toggle.setAttribute("data-current-theme", this.themePreference);
-    this.$toggle.innerText = this.themeOptions[this.themePreference];
-    this.$toggle.bindClick = this.onClick.bind(this);
-    this.$toggle.addEventListener("click", $toggle.bindClick);
-    localStorage.setItem("theme-preference", this.themePreference);
+  setToggleState(theme) {
+    this.$toggle.setAttribute("data-current-theme", theme);
+    this.$toggle.innerHTML = `<span class="kim-!-sr">Current theme: </span>${this.themeOptions[theme]}<span class="kim-!-sr">. Click to switch theme.</span>`;
+
+    localStorage.setItem("theme-preference", theme);
+    this.themePreference = theme;
   }
 }
 
