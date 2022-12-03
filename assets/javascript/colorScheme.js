1a3d000a5fcfc7ae41ee3bf74c0ebc1fcf36a915
Kimberly Grey <hi+github@berly.kim>
2022-12-03 17:07:10 +0000

    :bug: Fix color scheme code producing an error on pages with no toggle button

--- assets/javascript/colorScheme.js
+++ assets/javascript/colorScheme.js
@@ -3,6 +3,10 @@ const $d = document.documentElement;
 const $toggleButton = document.getElementById("toggle-color-scheme");
 const prefersLight = matchMedia("(prefers-color-scheme: light)");
 
+if ($toggleButton) {
+  initColorScheme();
+}
+
 // Set current color scheme
 function setColorScheme(colorScheme) {
   localStorage.setItem("prefers-color-scheme", colorScheme);
@@ -13,23 +17,25 @@ function setColorScheme(colorScheme) {
   );
 }
 
-// Check to see if we already have a preferred scheme stored, if so, use it
-// If not, base the decision on the user's current light/dark mode preference
-// with the fallback being dark
-let currentColorScheme = "dark";
-if (localStorage.getItem("prefers-color-scheme")) {
-  currentColorScheme = localStorage.getItem("prefers-color-scheme");
-} else {
-  currentColorScheme = prefersLight.match ? "light" : "dark";
-}
-setColorScheme(currentColorScheme);
+function initColorScheme() {
+  // Check to see if we already have a preferred scheme stored, if so, use it
+  // If not, base the decision on the user's current light/dark mode preference
+  // with the fallback being dark
+  let currentColorScheme = "dark";
+  if (localStorage.getItem("prefers-color-scheme")) {
+    currentColorScheme = localStorage.getItem("prefers-color-scheme");
+  } else {
+    currentColorScheme = prefersLight.match ? "light" : "dark";
+  }
+  setColorScheme(currentColorScheme);
 
-// Set up the toggle button a11y
-$toggleButton.setAttribute(
-  "aria-pressed",
-  $d.dataset.colorScheme == "light" ? "true" : "false"
-);
-$toggleButton.addEventListener("click", () => {
-  setColorScheme($d.dataset.colorScheme === "light" ? "dark" : "light");
-});
-$toggleButton.removeAttribute("hidden");
+  // Set up the toggle button a11y
+  $toggleButton.setAttribute(
+    "aria-pressed",
+    $d.dataset.colorScheme == "light" ? "true" : "false"
+  );
+  $toggleButton.addEventListener("click", () => {
+    setColorScheme($d.dataset.colorScheme === "light" ? "dark" : "light");
+  });
+  $toggleButton.removeAttribute("hidden");
+}
