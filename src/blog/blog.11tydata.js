c46dbb46bf975ffe2ffdc1fa385b8e1ab9d8a544
Kimberly Grey <kimberly.grey@digital.cabinet-office.gov.uk>
2023-01-11 20:25:25 +0000

    :sparkles: Add support for draft blog posts which don't get built

---
+++ src/blog/blog.11tydata.js
@@ -0,0 +1,26 @@
+const isProduction = process.env.ENVIRONMENT === "prod";
+
+const excludeFromProductionBuild = (data) => {
+  return isProduction && data.draft;
+};
+
+module.exports = {
+  layout: "blog-post",
+  tags: ["blog"],
+  mastheadBreadcrumbs: [
+    {
+      url: "/blog/",
+      label: "blog",
+    },
+  ],
+  eleventyComputed: {
+    eleventyExcludeFromCollections: (data) => {
+      return excludeFromProductionBuild(data)
+        ? true
+        : data.eleventyExcludeFromCollections;
+    },
+    permalink: (data) => {
+      return excludeFromProductionBuild(data) ? false : data.permalink;
+    },
+  },
+};
