5f78189e97e2d101bb5b25cbc05ca9dc556717ca
Kimberly Grey <hi+github@berly.kim>
2023-05-03 21:42:54 +0100

    :recycle: Move some repeated data into a global data file

--- src/_data/site.js
+++ src/_data/site.js
@@ -3,6 +3,9 @@ const environment = process.env.ENVIRONMENT;
 module.exports = function () {
   return {
     name: "beeps",
+    blogName: "beeps' blog",
+    authorName: "beeps",
+    authorEmail: "hi@berly.kim",
     domain: environment === "prod" ? "https://beeps.website" : "",
     environment,
   };
