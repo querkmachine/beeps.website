4779eaec79a257aa190afc879337bb8bbb5b5667
beeps <hi+github@berly.kim>
2024-02-01 21:52:08 +0000

    :zap: Cachebust OpenGraph image generation

--- config/opengraph.js
+++ config/opengraph.js
@@ -1,5 +1,9 @@
+const { DateTime } = require("luxon");
+
 const urlizeOpenGraphImage = function (url) {
-  return `https://v1.screenshot.11ty.dev/${encodeURIComponent(url)}/opengraph/`;
+  return `https://v1.screenshot.11ty.dev/${encodeURIComponent(
+    url
+  )}/opengraph/_${DateTime.local().toFormat("X")}/`;
 };
 
 module.exports = {
