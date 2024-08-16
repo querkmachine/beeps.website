2dacc77570cd6c9ee1259d986a9d5392db7f3928
beeps <hi+github@berly.kim>
2024-08-16 09:30:31 +0100

    :sparkles: Add webrings to homepage footer

---
+++ src/_data/webrings.js
@@ -0,0 +1,16 @@
+module.exports = function () {
+  return [
+    {
+      name: "Fediring",
+      url: "https://fediring.net",
+      nextUrl: "https://fediring.net/next?host=beeps.website",
+      previousUrl: "https://fediring.net/previous?host=beeps.website",
+    },
+    {
+      name: "Roboring",
+      url: "https://stellophiliac.github.io/roboring",
+      nextUrl: "https://stellophiliac.github.io/roboring/beeps/next",
+      previousUrl: "https://stellophiliac.github.io/roboring/beeps/previous",
+    },
+  ];
+};
