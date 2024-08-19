b6e64e36559d8544c8b68a453fdec644484e752e
beeps <hi+github@berly.kim>
2024-08-19 21:07:19 +0100

    :recycle: Refactor .well-known/button generation
    Also add a new button!

--- src/_data/buttons.js
+++ src/_data/buttons.js
@@ -1,31 +1,55 @@
 const buttonsBeeps = [
   {
+    src: "/assets/images/88x31-d.gif",
+    alt: "Dark blue and green animated button with Ash the amphimorpho",
+    schemaAlt: "beeps",
+    colorScheme: "dark",
+    motion: "high",
+  },
+  {
     src: "/assets/images/88x31-n.gif",
     alt: "Dark blue and green animated button with Emy the robat",
+    schemaAlt:
+      "beeps.website - a site by a plural, furry, otherkin weirdo about web dev, identity and stuff",
+    colorScheme: "dark",
+    motion: "minimal",
   },
   {
     src: "/assets/images/88x31.gif",
     alt: "Dark grey and orange animated button with Emy the robat",
+    schemaAlt:
+      "beeps.website - a site by a plural, furry, otherkin weirdo about web dev, identity and stuff",
+    colorScheme: "dark",
+    motion: "high",
   },
   {
     src: "/assets/images/88x31-n-static.png",
     alt: "Dark blue and green static button with Emy the robat",
+    schemaAlt: "beeps.website",
+    colorScheme: "dark",
   },
   {
     src: "/assets/images/88x31-static.png",
     alt: "Dark grey and orange static button with Emy the robat",
+    schemaAlt: "beeps.website",
+    colorScheme: "dark",
   },
   {
     src: "/assets/images/88x31-a-static.png",
     alt: "Dark blue and green static button with Ash the amphimorpho and 'beeps was here' text",
+    schemaAlt: "beeps was here",
   },
   {
     src: "/assets/images/88x31-r-static.png",
     alt: "90s Microsoft Windows style grey button with Emy the robat and 'robot bat NOW!' text",
+    schemaAlt: "Robot bat NOW!",
+    colorScheme: "light",
   },
   {
     src: "/assets/images/88x31-w-static.png",
     alt: "W3C validator style button with robat symbol, 'beeps 2.0' text and a checkmark",
+    schemaAlt: "Valid beeps 2.0",
+    colorScheme: "light",
   },
 ];
 
