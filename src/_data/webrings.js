f2faecc2a4c82a0032ac97df163f5f7d8085623c
beeps <hi+github@berly.kim>
2024-09-26 19:35:17 +0100

    :card_file_box: Add new webring

--- src/_data/webrings.js
+++ src/_data/webrings.js
@@ -1,16 +1,22 @@
 module.exports = function () {
   return [
     {
-      name: "Fediring",
-      url: "https://fediring.net",
-      nextUrl: "https://fediring.net/next?host=beeps.website",
-      previousUrl: "https://fediring.net/previous?host=beeps.website",
+      name: "Noodle Ring",
+      url: "https://noodle-r.ing/",
+      nextUrl: "https://noodle-r.ing/beeps/next",
+      previousUrl: "https://noodle-r.ing/beeps/prev",
     },
     {
       name: "Roboring",
       url: "https://stellophiliac.github.io/roboring",
       nextUrl: "https://stellophiliac.github.io/roboring/beeps/next",
       previousUrl: "https://stellophiliac.github.io/roboring/beeps/previous",
+    },
+    {
+      name: "Fediring",
+      url: "https://fediring.net",
+      nextUrl: "https://fediring.net/next?host=beeps.website",
+      previousUrl: "https://fediring.net/previous?host=beeps.website",
     },
   ];
 };
