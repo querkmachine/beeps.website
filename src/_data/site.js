2c8a8d840782bdd46fb9119231a6ea085ab41fce
beeps <hi+github@berly.kim>
2024-09-26 19:35:47 +0100

    :speech_balloon: Update footer link text

--- src/_data/site.js
+++ src/_data/site.js
@@ -16,7 +16,7 @@ module.exports = function () {
     ],
     footerLinks: [
       { href: "/about/", text: "About beeps" },
-      { href: "/ash/", text: "Reference sheet" },
+      { href: "/ash/", text: "Ash the amphimorpho" },
       { href: "/brand/", text: "beeps' brand" },
       { href: "/contact/", text: "Contact beeps" },
     ],
