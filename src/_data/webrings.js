86531a190ccf99bc9e886646bb2ec825d95c81e6
beeps <hi+github@berly.kim>
2024-09-27 10:18:02 +0100

    :bento: Add icons for webrings

--- src/_data/webrings.js
+++ src/_data/webrings.js
@@ -1,18 +1,21 @@
 module.exports = function () {
   return [
     {
+      icon: "webring-noodlering.svg",
       name: "Noodle Ring",
       url: "https://noodle-r.ing/",
       nextUrl: "https://noodle-r.ing/beeps/next",
       previousUrl: "https://noodle-r.ing/beeps/prev",
     },
     {
+      icon: "webring-roboring.svg",
       name: "Roboring",
       url: "https://stellophiliac.github.io/roboring",
       nextUrl: "https://stellophiliac.github.io/roboring/beeps/next",
       previousUrl: "https://stellophiliac.github.io/roboring/beeps/previous",
     },
     {
+      icon: "webring-fediring.svg",
       name: "Fediring",
       url: "https://fediring.net",
       nextUrl: "https://fediring.net/next?host=beeps.website",
