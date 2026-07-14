4c81ae5288812b446822521df8ac0ea3f1dace3c
beeps <hi+github@berly.kim>
2026-07-14 09:02:31 +0100

    :card_file_box: Remove old colour scheme 88x31 buttons
    Remove them from display on the website and inclusion in the the .well-known/buttons.json file.
    
    Manually specify the schema IDs so that these don't change as a result of the removals.
    
    The files themselves remain just in case they've been hotlinked from anywhere.

--- src/_data/buttons/beeps.js
+++ src/_data/buttons/beeps.js
@@ -1,5 +1,6 @@
 export default [
   {
+    id: "0",
     src: "/assets/images/88x31-d.gif",
     alt: "Dark blue and green animated button with Ash the amphimorpho",
     schemaAlt: "beeps",
@@ -7,11 +8,13 @@ export default [
     motion: "high",
   },
   {
+    id: "1",
     src: "/assets/images/88x31-a-static.png",
     alt: "Dark blue and green static button with Ash the amphimorpho and 'beeps was here' text",
     schemaAlt: "beeps was here",
   },
   {
+    id: "2",
     src: "/assets/images/88x31-n.gif",
     alt: "Dark blue and green animated button with Emy the robat",
     schemaAlt:
@@ -20,32 +23,21 @@ export default [
     motion: "minimal",
   },
   {
-    src: "/assets/images/88x31.gif",
-    alt: "Dark grey and orange animated button with Emy the robat",
-    schemaAlt:
-      "beeps.website - a site by a plural, furry, otherkin weirdo about web dev, identity and stuff",
-    colorScheme: "dark",
-    motion: "high",
-  },
-  {
+    id: "4",
     src: "/assets/images/88x31-n-static.png",
     alt: "Dark blue and green static button with Emy the robat",
     schemaAlt: "beeps.website",
     colorScheme: "dark",
   },
   {
-    src: "/assets/images/88x31-static.png",
-    alt: "Dark grey and orange static button with Emy the robat",
-    schemaAlt: "beeps.website",
-    colorScheme: "dark",
-  },
-  {
+    id: "6",
     src: "/assets/images/88x31-r-static.png",
     alt: "90s Microsoft Windows style grey button with Emy the robat and 'robot bat NOW!' text",
     schemaAlt: "Robot bat NOW!",
     colorScheme: "light",
   },
   {
+    id: "7",
     src: "/assets/images/88x31-w-static.png",
     alt: "W3C validator style button with robat symbol, 'beeps 2.0' text and a checkmark",
     schemaAlt: "Valid beeps 2.0",
